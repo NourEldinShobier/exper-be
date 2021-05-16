79c34b020d92d12e0c04f5af7697c189609856c8
Nour El-Din Shobier <nour.eldin.shobier@gmail.com>
2021-05-16 13:44:45 +0200

    feat(core: users): :sparkles: add dtos of users crud operations

--- src/core/features/users/dto/crud.dto.ts
+++ src/core/features/users/dto/crud.dto.ts
@@ -1,5 +1,65 @@
-export class CreateUserDto { }
+import {
+    ArrayMaxSize, ArrayNotEmpty, IsNotEmpty, IsNumber, IsOptional, IsString, MaxLength, Min
+} from "class-validator";
 
-export class UpdateUserDto { }
+export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(50)
+    readonly name: string;
 
-export class DeleteUserDto { }
+    @IsOptional()
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(50)
+    readonly phone?: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(50)
+    readonly username: string;
+
+    @IsString()
+    @IsNotEmpty()
+    readonly password: string;
+
+    @IsString({ each: true })
+    @ArrayNotEmpty()
+    @ArrayMaxSize(6)
+    readonly role: string[];
+}
+
+export class UpdateUserDto {
+
+    //@IsJWT()
+    //readonly jwt: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(50)
+    readonly name: string;
+
+    @IsOptional()
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(50)
+    readonly phone?: string;
+
+    @IsString()
+    @IsNotEmpty()
+    readonly password: string;
+
+    @IsString({ each: true })
+    @ArrayNotEmpty()
+    @ArrayMaxSize(6)
+    readonly role: string[];
+}
+
+export class DeleteUserDto {
+    //@IsJWT()
+    //readonly jwt: string;
+
+    @IsNumber()
+    @Min(0)
+    readonly id: number;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { PORT } from 'src/infra/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true
+  }));
   await app.listen(PORT);
 }
 bootstrap();
