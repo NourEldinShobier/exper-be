0e8f980d06fec6755a1334f5b9ac62e8023d5a5d
Nour El-Din Shobier <nour.eldin.shobier@gmail.com>
2021-05-20 02:47:14 +0200

    feat(server): :sparkles: add api version and docs to app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { API_VERSION, DOCS } from 'src/server/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {
+  }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('docs')
+  getDocs(): string {
+    return DOCS;
+  }
+
+  @Get('version')
+  getAPIVersion(): string {
+    return API_VERSION;
   }
 }
--- src/server/config/environments.ts
+++ src/server/config/environments.ts
@@ -4,6 +4,7 @@ import { ENV } from '../constants';
 
 dotenv.config();
 
-
-export const NODE_ENV: string = process.env.NODE_ENV || ENV.DEVELOPMENT
-export const PORT: number = +process.env.PORT || 3000
+export const API_VERSION: string = process.env.API_VERSION || '1.0';
+export const NODE_ENV: string = process.env.NODE_ENV || ENV.DEVELOPMENT;
+export const PORT: number = +process.env.PORT || 3000;
+export const DOCS: string = process.env.DOCS || '';
